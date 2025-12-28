8b4695d2375c5cb4b34bf2506121ae0ddafe425e
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-12-28 18:34:47 +0545

    Setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-blue-500">Hello Vite + React!</div>
+    </>
+  );
+}
+
+export default App;
