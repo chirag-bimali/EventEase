66df6ff9a18d6d403bd078fc721fe1e19663bc5f
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-12-29 22:14:25 +0545

    Create a library for prisma client

---
+++ backend/src/lib/primsa.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+if (
+  process.env.DATABASE_HOST === undefined ||
+  process.env.DATABASE_USER === undefined ||
+  process.env.DATABASE_PASSWORD === undefined ||
+  process.env.DATABASE_NAME === undefined
+) {
+  throw new Error("Database environment variables are not set properly.");
+}
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
