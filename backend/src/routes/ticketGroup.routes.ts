d2df1b8409a5eb0a49176d00a10d45a1b4cb1bd6
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-01-01 16:37:10 +0545

    Add routes for ticket groups

---
+++ backend/src/routes/ticketGroup.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createTicketGroup, deleteTicketGroup, getTicketGroupsByEvent, updateTicketGroup, getTicketGroupAvailability } from "../controllers/ticketGroup.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+export const ticketGroupRouter = Router();
+// Ticket group routes
+ticketGroupRouter.post("/", authMiddleware, createTicketGroup);
+ticketGroupRouter.get("/event/:eventId", getTicketGroupsByEvent);
+ticketGroupRouter.get("/:id/availability", getTicketGroupAvailability);
+ticketGroupRouter.delete("/:id", authMiddleware, deleteTicketGroup);
+ticketGroupRouter.patch("/:id", authMiddleware, updateTicketGroup);
